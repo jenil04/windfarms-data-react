e018a1aac959fc8600b6da08f9f29739a1e45e50
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2018-10-06 13:57:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Mantainance Data (Ensemble Coding Challenge)</h1>
+          <h1 className="App-title">Mantainance Data</h1>
         </header>
         <p className="App-intro">
           Author - Jenil Thakker
